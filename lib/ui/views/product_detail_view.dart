25cb8d34bbfad5db2599e963f216a9f4f6ed6346
Hussein Mkwizu <hussein@azampay.com>
2020-09-27 17:48:12 +0300

    image previews for products

--- lib/ui/views/product_detail_view.dart
+++ lib/ui/views/product_detail_view.dart
@@ -14,6 +14,8 @@ import 'package:online_china_app/ui/widgets/details_header.dart';
 import 'package:online_china_app/ui/widgets/product_attribute.dart';
 import 'package:online_china_app/ui/widgets/product_options_modal.dart';
 import 'package:online_china_app/ui/widgets/quantity_input.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
 import 'package:provider/provider.dart';
 import 'package:online_china_app/core/enums/constants.dart';
 
@@ -140,15 +142,21 @@ class _ProductDetailViewState extends State<ProductDetailView> {
                                           int itemIndex) {
                                         var imageItem =
                                             product.images[itemIndex];
-                                        return CachedNetworkImage(
-                                          imageUrl: imageItem.src != null
-                                              ? imageItem.src
-                                              : "",
-                                          fit: BoxFit.contain,
-                                          placeholder: (context, url) =>
-                                              Image.asset(
-                                            PLACEHOLDER_IMAGE,
+                                        return InkWell(
+                                          onTap: () {
+                                            openInGallery(context,
+                                                product.images, itemIndex);
+                                          },
+                                          child: CachedNetworkImage(
+                                            imageUrl: imageItem.src != null
+                                                ? imageItem.src
+                                                : "",
                                             fit: BoxFit.contain,
+                                            placeholder: (context, url) =>
+                                                Image.asset(
+                                              PLACEHOLDER_IMAGE,
+                                              fit: BoxFit.contain,
+                                            ),
                                           ),
                                         );
                                       },
@@ -404,6 +412,23 @@ class _ProductDetailViewState extends State<ProductDetailView> {
       Navigator.pushNamed(context, '/order_address', arguments: params);
     }
   }
+
+  void openInGallery(
+      BuildContext context, List<ImageItem> galleryItems, final int index) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => GalleryPhotoViewWrapper(
+          galleryItems: galleryItems,
+          backgroundDecoration: const BoxDecoration(
+            color: Colors.black,
+          ),
+          initialIndex: index,
+          scrollDirection: Axis.horizontal,
+        ),
+      ),
+    );
+  }
 }
 
 class BadgeIcon extends StatelessWidget {
@@ -442,6 +467,117 @@ class BadgeIcon extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class GalleryPhotoViewWrapper extends StatefulWidget {
+  GalleryPhotoViewWrapper({
+    this.loadingBuilder,
+    this.backgroundDecoration,
+    this.minScale,
+    this.maxScale,
+    this.initialIndex,
+    @required this.galleryItems,
+    this.scrollDirection = Axis.horizontal,
+  }) : pageController = PageController(initialPage: initialIndex);
+
+  final LoadingBuilder loadingBuilder;
+  final Decoration backgroundDecoration;
+  final dynamic minScale;
+  final dynamic maxScale;
+  final int initialIndex;
+  final PageController pageController;
+  final List<ImageItem> galleryItems;
+  final Axis scrollDirection;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _GalleryPhotoViewWrapperState();
+  }
+}
+
+class _GalleryPhotoViewWrapperState extends State<GalleryPhotoViewWrapper> {
+  int currentIndex;
+
+  @override
+  void initState() {
+    currentIndex = widget.initialIndex;
+    super.initState();
+  }
+
+  void onPageChanged(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: widget.backgroundDecoration,
+        constraints: BoxConstraints.expand(
+          height: MediaQuery.of(context).size.height,
+        ),
+        child: Stack(
+          children: <Widget>[
+            PhotoViewGallery.builder(
+              scrollPhysics: const BouncingScrollPhysics(),
+              builder: _buildItem,
+              itemCount: widget.galleryItems.length,
+              loadingBuilder: widget.loadingBuilder,
+              backgroundDecoration: widget.backgroundDecoration,
+              pageController: widget.pageController,
+              onPageChanged: onPageChanged,
+              scrollDirection: widget.scrollDirection,
+            ),
+            Container(
+              padding: const EdgeInsets.all(18.0),
+              child: IconButton(
+                onPressed: () => Navigator.pop(context),
+                icon: Icon(
+                  Icons.close,
+                  color: Colors.white,
+                  size: 30,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Container(
+                padding: const EdgeInsets.all(18.0),
+                child: Text(
+                  "${currentIndex + 1} / ${widget.galleryItems.length}",
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 17.0,
+                    decoration: null,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  PhotoViewGalleryPageOptions _buildItem(BuildContext context, int index) {
+    final ImageItem item = widget.galleryItems[index];
+    return PhotoViewGalleryPageOptions(
+      imageProvider: item.src != null && item.src.isNotEmpty
+          ? CachedNetworkImageProvider(
+              item.src,
+            )
+          : Image.asset(
+              PLACEHOLDER_IMAGE,
+              fit: BoxFit.contain,
+            ),
+      initialScale: PhotoViewComputedScale.contained,
+      // minScale: PhotoViewComputedScale.contained * (0.5 + index / 10),
+      // maxScale: PhotoViewComputedScale.covered * 4.1,
+      heroAttributes: PhotoViewHeroAttributes(tag: item.id),
     );
   }
 }
